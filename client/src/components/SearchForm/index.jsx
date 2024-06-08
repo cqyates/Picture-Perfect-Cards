d15ad2bd58a78d03357964d3741213dddd818ea7
Corey Yates <cqyates@gmail.com>
2024-06-08 19:24:32 -0400

    set up search input and button

---
+++ client/src/components/SearchForm/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchForm = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setSearchInput(event.target.value);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("Hello");
+  }
+
+  return (
+    <Form style={{ display: 'flex' }}>
+      <Form.Group>
+        <Form.Control
+          name="searchInput"
+          value={searchInput}
+          onChange={(e) => handleInputChange(e)}
+          type="text"
+          size="lg"
+          placeholder="Enter Search Term"
+        />
+        <Button onClick={handleFormSubmit}>Search</Button>
+      </Form.Group>
+    </Form>
+  );
+};
+export default SearchForm;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,37 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
-}
+import {useState} from "react"
 
-export default Home
+import { Row, Col, Container, Form, Button } from 'react-bootstrap';
+
+
+const Home = () => {
+  const [searchInput, setSearchInput] = useState("")
+  const handleSearch = () => {
+   console.log(searchInput)
+   // run pexel fetch through the backend 
+  };
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setSearchInput(event.target.value)
+  }
+  return (
+    <Container>
+      <Row>
+        <Col lg={3}>
+          <Form.Group>
+            <Form.Control
+             type="text"
+             value={searchInput}
+             onChange={handleInputChange}
+             name="search-input"
+             />
+            <Button onClick={handleSearch} type="button">
+              Search
+            </Button>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
