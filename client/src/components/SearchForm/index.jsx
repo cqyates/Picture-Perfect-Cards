ad2937b4b2a6f0c2cb0ad594b69ab971e7d90ddf
Corey Yates <cqyates@gmail.com>
2024-06-08 19:27:53 -0400

    added back font awesome search icon

--- client/src/components/SearchForm/index.jsx
+++ client/src/components/SearchForm/index.jsx
@@ -3,8 +3,7 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 const SearchForm = () => {
   const [searchInput, setSearchInput] = useState('');
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,31 +1,33 @@
-import {useState} from "react"
+import { useState } from 'react';
 
 import { Row, Col, Container, Form, Button } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
-  const [searchInput, setSearchInput] = useState("")
+  const [searchInput, setSearchInput] = useState('');
   const handleSearch = () => {
-   console.log(searchInput)
-   // run pexel fetch through the backend 
+    console.log(searchInput);
+    // run pexel fetch through the backend
   };
   const handleInputChange = (event) => {
-    console.log(event.target.value)
-    setSearchInput(event.target.value)
-  }
+    console.log(event.target.value);
+    setSearchInput(event.target.value);
+  };
   return (
     <Container>
       <Row>
-        <Col lg={3}>
-          <Form.Group>
+        <Col lg={3} sm={6}>
+          <Form.Group style={{display: "flex"}}>
             <Form.Control
-             type="text"
-             value={searchInput}
-             onChange={handleInputChange}
-             name="search-input"
-             />
+              type="text"
+              value={searchInput}
+              onChange={handleInputChange}
+              name="search-input"
+            />
             <Button onClick={handleSearch} type="button">
-              Search
+              <FontAwesomeIcon icon={faSearch} />
             </Button>
           </Form.Group>
         </Col>
